fe73c5ccc9eaa409f6d27036f2eecc5573626002
hhc722130 <aoaand@naver.com>
2025-07-02 10:59:13 +0900

    no message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,17 @@ public class PlayerMovementController : MonoBehaviour
     }
     void Update()
     {
+        float xInput = Input.GetAxis("Horizontal");
+        Debug.Log("xInput: " + xInput);
+        float zInput = Input.GetAxis("Vertical");
+        Debug.Log("zInput: " + zInput);
+
+        float xspeed = xInput * speed;
+        float zspeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xspeed, 0f, zspeed);
+        playerRigidbody.linearVelocity = newVelocity;
+
         if (Input.GetKey(KeyCode.UpArrow) == true)
         {
             playerRigidbody.AddForce(0f, 0f, speed);
