8c097221cd34c939bdddfa0d01de859f299547b1
hhc722130 <aoaand@naver.com>
2025-07-02 17:16:26 +0900

    no message

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+/*using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public class Rotator : MonoBehaviour
+    {
+        public float rotationSpeed =60f;
+
+        void Update()
+        {
+            // Rotate the object around its local Y axis at the specified speed
+            transform.Rotate(0f,  * Time.deltaTime);
+        }
+    }
+}*/
