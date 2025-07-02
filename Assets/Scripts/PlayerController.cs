4142f734cec31493934753fdd4772a3d72cb66c1
hhc722130 <aoaand@naver.com>
2025-07-02 10:42:15 +0900

    no message

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
+    }
+}
